52d743fa7e8b2936764be0061debe620485cec64
SonMyungjae <audwo2tp@gmail.com>
2022-10-14 16:01:36 +0900

    최종수정

--- Samples/02.sample-03.py
+++ Samples/02.sample-03.py
@@ -2,8 +2,13 @@ from gtts import gTTS
 from playsound import playsound
 import os
 
-text = '안녕하세요 마이크로소프트 에이아이 스쿨에 오신 것을 환경합니다.'
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-tts=gTTS(text = text, lang='ko')
+text = '안녕하세요 마이크로소프트 에이아이 스쿨에 오신 것을 환영합니다.'
 
-tts.save('hi.mp3')
+tts = gTTS(text = text, lang='ko')
+
+current_file_path = os.getcwd() + "./h1.mp3"
+tts.save(current_file_path)
+
+playsound(current_file_path)
